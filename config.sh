c7da5707fe441d94c2a3d84336db661d1f4a1e12
jazi0 <mbarbeau@gmail.com>
2022-11-04 20:46:36 -0400

    Test 2

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt-get update && sudo apt-get install spotify-client
 
-#Firefox Deb
+#Firefox Deb 1.0
 sudo snap remove firefox
 sudo add-apt-repository ppa:mozillateam/ppa
 echo '
